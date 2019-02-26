89d2cba09e6d0aac288bf4c4c4c91477ee46595b
David Anderson <developing.david@gmail.com>
2019-02-26 13:48:51 -0700

    3.13: Getting Input from user done

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,3 +4,17 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+let command = process.argv[2];
+
+if (command === 'add') {
+	console.log('Adding a note.');
+} else if (command === 'list') {
+	console.log('Listing all notes.');
+} else if (command === 'read') {
+	console.log('Reading note.');
+} else if (command === 'remove') {
+	console.log('Removing note.');
+} else {
+	console.log('Command not recognized.');
+}
