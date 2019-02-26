8db59d694724b248b5c3f7f9c02f8f7c8c0b21cf
David Anderson <developing.david@gmail.com>
2019-02-26 13:23:07 -0700

    3.12: Finish video try nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,16 +1,6 @@
 console.log('\x1b[36m%s\x1b[0m', 'Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
 
 const notes = require('./notes');
-
-console.log(_.uniq(['David', 1, 'David', 1, 2, 3, 4, '1']));
-
-// let user = os.userInfo();
-
-// fs.appendFileSync(
-// 	'greetings.txt',
-// 	`Hello ${user.username}! You are ${notes.age}.`
-// );
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,8 +1,5 @@
 console.log('\x1b[36m%s\x1b[0m', 'Starting notes.js');
 
-module.exports.add = (a, b) => a + b;
-
 module.exports.addNote = () => {
-	console.log('add note');
-	return 'New note';
+	return '';
 };
